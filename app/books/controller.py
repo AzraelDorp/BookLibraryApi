115cde7aaffb992bff8f3a8ac2e0d205267c7383
Matt Vail <matthewvail@hotmail.com>
2023-05-24 22:02:14 -0700

    chore: update file names

---
+++ app/books/controller.py
@@ -0,0 +1,11 @@
+from flask_restx import Namespace, Resource
+
+api = Namespace("books", description="")
+
+# plural for many
+@api.route("")
+class PeopleAPI(Resource):
+    @api.doc(responses={"200": "OK"})
+    def get(self):
+        '''Returns all people with filters'''
+        return {"success", 200}
