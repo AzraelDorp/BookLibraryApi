e334d5f3dc9686a52945e4aca486b8be0af2f86e
Matt Vail <matthewvail@hotmail.com>
2023-07-02 22:47:41 -0700

    feat: add the base route and template examples for local testing

--- app/books/controller.py
+++ app/books/controller.py
@@ -1,6 +1,6 @@
 from bson import ObjectId
 from datetime import datetime
-from flask import request
+from flask import request, render_template, make_response, url_for, flash, redirect
 from flask_restx import Namespace, Resource
 from flask_accepts import accepts, responds
 from .model import BookModel
@@ -12,35 +12,35 @@ TODO: Need to add the correct error handling for each of these endpoints
 TODO: Need to add the correct response codes for each of these endpoints
 TODO: Need to add the correct documentation for each of these endpoints
 TODO: Need to add the correct authentication for each of these endpoints
-
+TODO: Need to update all books from request.get_json() to data = request.get_json() and then use data["key"] to get the value
 """
 api = Namespace(name="books", description="")
 
 # endpoints for all books
 
-@api.route("")
+
+@api.route("/")
 class BookAPI(Resource):
-    @responds(schema=BookSchemaResponse, api=api)
+    @responds(schema=BookSchemaResponse, api=api, status_code=201)
     @api.doc(responses={"200": "OK"})
     def get(self):
         '''Returns all Books with filters'''
-        # books = DatabaseModel().getAllItems("Books")
         books = BookModel().getAllBooks()
-        print(books)
-        return  {"books": books}
-    
+        return {"books": books}
+
+
     # build a post functions for the the book route
     @api.doc(responses={"201": "Created"})
     @accepts(schema=BookSchemaPost, api=api)
     @responds(schema=BookSchemaPost, api=api, status_code=201)
     def post(self):
         '''Creates a new Book'''
-        
+
         books = request.get_json()
 
         # add book defaults
-        # add a book_id to the book
         books["book_id"] = ObjectId()
+
         # add a created_at and updated_at to the book
         books["created_at"] = datetime.now()
         books["updated_at"] = datetime.now()
@@ -55,13 +55,14 @@ class BookAPI(Resource):
 @api.response(404, "Book not found")
 class Book(Resource):
     @responds(schema=BookSchema, api=api)
-
     @api.doc(responses={"200": "OK"})
     def get(self, id):
-        '''Returns a Book with id'''
+        '''Returns a single Book'''
         book = BookModel().getBookByID(id)
         return book
 
+# this endpoint needs to be revisited and updated
+# do i want it to create a book or just update it
 # add a put function for the book route
     @api.doc(responses={"200": "OK"})
     @accepts(schema=BookSchemaPost, api=api)
@@ -70,11 +71,12 @@ class Book(Resource):
         '''Updates a Book with id if it exists, otherwise creates a new Book'''
 
         dbBook = BookModel().getBookByID(id)
-        # add a created_at and updated_at to the book
+
+        # if the book exists then update it
         if dbBook:
             print("book exists")
             book = dbBook
-            book["updated_at"] = datetime.now()
+            book["updated_at"] = datetime.now() # type: ignore
             # loop through the request body and update the book
             for key in request.get_json().keys():
                 book[key] = request.get_json()[key]
@@ -82,6 +84,7 @@ class Book(Resource):
             book = BookModel().updateBookByID(id, book)
             return book
         else:
+            '''if the book does not exist then create it'''
             print("book does not exist")
             book = request.get_json()
             book["book_id"] = id
@@ -93,5 +96,100 @@ class Book(Resource):
     @api.doc(responses={"204": "No Content"})
     def delete(self, id):
         '''Deletes a Book with id'''
-        BookModel().deleteBook(id)
+        book = BookModel().getBookByID(id)
+
+        # if the book exists then delete it
+        if book:
+            BookModel().deleteBook(book['book_id']) # type: ignore
+        else:
+            return {"message": "Book does not exist"}, 404
         return None, 204
+
+
+# endpoints for internal use only
+# these endpoints are not exposed to the user
+# these are used to test functionality
+
+"""
+@api.route("/")
+class BookAPI(Resource):
+    @responds( api=api)
+    @api.doc(responses={"200": "OK"})
+    def get(self):
+        '''Returns all Books with filters'''
+        # books = DatabaseModel().getAllItems("Books")
+        books = BookModel().getAllBooks()
+        #print(books)
+        
+        #return  {"books": books}
+        headers = {'Content-Type': 'text/html'}
+        return make_response( render_template('index.html', books=books),headers) 
+    
+    # # build a post functions for the the book route
+    # @api.doc(responses={"201": "Created"})
+    # @accepts(schema=BookSchemaPost, api=api)
+    # @responds(schema=BookSchemaPost, api=api, status_code=201)
+    # def post(self):
+    #     '''Creates a new Book'''
+        
+    #     books = request.get_json()
+
+    #     # add book defaults
+    #     # add a book_id to the book
+    #     books["book_id"] = ObjectId()
+    #     # add a created_at and updated_at to the book
+    #     books["created_at"] = datetime.now()
+    #     books["updated_at"] = datetime.now()
+
+    #     books = BookModel().createBook(books)
+    #     return books
+
+@api.route("/newbook/")
+class BookCreate(Resource):
+    @responds( api=api)
+    @api.doc(responses={"200": "OK"})
+    def get(self):
+        '''Returns all Books with filters'''
+        # books = DatabaseModel().getAllItems("Books")
+        # books = BookModel().getAllBooks()
+        #print(books)
+        
+        #return  {"books": books}
+        headers = {'Content-Type': 'text/html'}
+        return make_response( render_template('create.html'),headers)
+    
+    # build a post functions for the the book route
+    @api.doc(responses={"201": "Created"})
+    @accepts( api=api)
+    @responds(schema=BookSchemaPost, api=api, status_code=201)
+    def post(self):
+        '''Creates a new Book'''
+        headers = {'Content-Type': 'text/html'}
+
+        books = {}
+        form = request.form
+        for i in form:
+            authors = []
+            if i == "authors":
+                for author in form[i].split(","):
+                    authors.append(author)
+                books[i] = authors
+            else:
+                books[i] = form[i]
+
+        #print(books)
+
+        # add book defaults
+        # add a book_id to the book
+        books["book_id"] = str(ObjectId())
+        # add a created_at and updated_at to the book
+        books["created_at"] = str(datetime.now())
+        books["updated_at"] = str(datetime.now())
+
+        response = BookModel().createBook(books)
+        if response.get("ResponseMetadata").get("HTTPStatusCode") == 200:
+            return  make_response( render_template('success.html', books=books),headers)
+        #print(books)
+
+        return make_response( render_template('success.html', books=books),headers)
+ """
